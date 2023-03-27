bb9a2fa2840ba5e2899db85bf4cb3d8453334123
AhmedSaIah <ahmedsalah5446@gmail.com>
2023-03-27 21:34:16 +0200

    Screens updated & firebase fix & delete images

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { React } from "react";
-import { StyleSheet, Text, View, TouchableOpacity, ImageBackground } from "react-native";
+import { StyleSheet,ImageBackground, Text, View, TouchableOpacity } from "react-native";
 
 export default function HomeScreen({ navigation }) {
   return (
@@ -9,72 +9,81 @@ export default function HomeScreen({ navigation }) {
       style={styles.background}
       source={require("../assets/talabatlogo.png")}>
       </ImageBackground>
+      <View>
+        <Text style={styles.headerText}>Already a user?</Text>
+      </View>
       <View style={styles.inputView}>
         <TouchableOpacity onPress={() => navigation.navigate("Login")}>
-          <Text style={styles.TextInput}>     Login     </Text>
+          <Text style={styles.TextInput}>       Login      </Text>
         </TouchableOpacity>
       </View>
       <View style={styles.inputView}>
         <TouchableOpacity onPress={() => navigation.navigate("Register")}> 
-          <Text style={styles.TextInput}>     Register      </Text>
+          <Text style={styles.TextInput}>       Register        </Text>
         </TouchableOpacity>
       </View>
-      {/* <TouchableOpacity onPress={() => navigation.navigate("Forgot")}>
-        <Text style={styles.forgot}>      Forgot Password?      </Text>
-      </TouchableOpacity> */}
+      <TouchableOpacity onPress={() => navigation.navigate("Forgot")}>
+        <Text style={styles.forgot}>        Forgot Password?        </Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    logo: {
-      aspectRatio: 2/1,
-      alignItems: 'center',
-      marginBottom: 40,
-    },
-    background:{
-      marginLeft:20,
-      marginBottom:20,
-      height: 80,
-      width: "95%",
-      alignItems: 'stretch',
-    },
-    inputView: {
-      backgroundColor: "#FF7000",
-      borderRadius: 30,
-      width: "90%",
-      height: 45,
-      marginBottom: 20,
-      alignItems: "center",
-    },
-    TextInput: {
-      height: 50,
-      flex: 1,
-      padding: 10,
-      marginLeft: 20,
-      fontSize: 15,
-      color: "white",
-    },
-    login:{
-      width: "30%",
-      borderRadius: 25,
-      height: 50,
-      alignItems:"center",
-      justifyContent:"center",
-      marginTop:40,
-      backgroundColor:"#FF7000",
-      color: "white",
-    },
-    forgot: {
-      height: 30,
-      marginBottom: 30,
-    },
-  });
-  
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "left",
+  },
+  logo: {
+    aspectRatio: 2 / 1,
+    alignItems: "center",
+    marginBottom: 40,
+  },
+  headerText: {
+    fontWeight: "bold",
+    fontSize: 20,
+    marginBottom: 15,
+  },
+  background:{
+    marginLeft:20,
+    marginBottom:20,
+    height: 82,
+    width: "95%",
+  },
+  inputView: {
+    backgroundColor: "#FF7000",
+    borderRadius: 10,
+    fontWeight: "bold",
+    fontSize: 50,
+    width: "90%",
+    height: 45,
+    marginBottom: 20,
+    padding: 10,
+    alignItems: "center",
+  },
+  TextInput: {
+    marginLeft: 20,
+    fontSize: 18,
+    color: "white",
+    textAlign:"left",
+    fontWeight: "bold",
+  },
+  login: {
+    width: "30%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#FF7000",
+    color: "white",
+  },
+  forgot: {
+    height: 30,
+    fontWeight: "bold",
+  },
+});
